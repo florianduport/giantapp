3a898bcaf7878ffb29f31451185de7afc5584d71
OpenShift guest <florianduport@gmail.com>
2014-04-17 15:50:31 -0400

    ajout de l'application

---
+++ AppWebcore/Application/my-app/platforms/android/bin/classes.dex.d
@@ -0,0 +1,9 @@
+C:\Users\Florian\Google Drive\MyApp\my-app\platforms\android\bin\classes.dex : \
+C:\Users\Florian\Google Drive\MyApp\my-app\platforms\android\bin\classes\com\phonegap\helloworld\BuildConfig.class \
+C:\Users\Florian\Google Drive\MyApp\my-app\platforms\android\bin\classes\com\phonegap\helloworld\HelloWorld.class \
+C:\Users\Florian\Google Drive\MyApp\my-app\platforms\android\bin\classes\com\phonegap\helloworld\R$attr.class \
+C:\Users\Florian\Google Drive\MyApp\my-app\platforms\android\bin\classes\com\phonegap\helloworld\R$drawable.class \
+C:\Users\Florian\Google Drive\MyApp\my-app\platforms\android\bin\classes\com\phonegap\helloworld\R$string.class \
+C:\Users\Florian\Google Drive\MyApp\my-app\platforms\android\bin\classes\com\phonegap\helloworld\R$xml.class \
+C:\Users\Florian\Google Drive\MyApp\my-app\platforms\android\bin\classes\com\phonegap\helloworld\R.class \
+C:\Users\Florian\Google Drive\MyApp\my-app\platforms\android\bin\dexedLibs\classes-18006d06bd5ba88fe76d0a4fc87ff3fb.jar \
