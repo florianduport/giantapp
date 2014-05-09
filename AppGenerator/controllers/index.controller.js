4a6bea5e9476b08764c836bae8f3c6a28c69d1ee
OpenShift guest <florianduport@gmail.com>
2014-05-09 11:38:16 -0400

    init cz 2

--- AppGenerator/controllers/index.controller.js
+++
@@ -1,6 +0,0 @@
-var model = require('../models/index.model');
-exports.initialize = function(req, res){
-    model.initialize(req, function(model){
-        res.render('./pages/index', {model: model});
-    });
-}
