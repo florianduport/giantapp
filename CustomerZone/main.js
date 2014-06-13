80ff21468d25d27852e5e585724a4892e9cd0108
florian <florianduport@gmail.com>
2014-06-14 00:11:40 +0200

    modif locale

--- CustomerZone/main.js
+++ CustomerZone/main.js
@@ -2,11 +2,11 @@ var express = require('express');
 var indexController = require('./controllers/index.controller');
 
 var app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 //routes / mapping controller
 app.get('/', indexController.initialize);
 
-app.listen(process.env.PORT);
+app.listen(3000);
