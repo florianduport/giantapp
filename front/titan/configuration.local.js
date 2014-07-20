197922a28733686d51ef2b48a571fdc3a4e56334
Florian Duport <florianduport@gmail.com>
2014-07-20 19:50:10 +0200

    config titan

---
+++ front/titan/configuration.local.js
@@ -0,0 +1,19 @@
+/**
+ * Surcharge de la configuration en BDD
+ * @class LocalConfig 
+ */
+var LocalConfig = {
+
+	/**
+	* Override attributes form db config here
+	*/
+	//pbligatoire
+	database : {
+		address : "mongodb://giantapp:Answer&&Pigeon2010@94.23.203.174:27017/giantapp"
+	},
+	addressBasePath : "http://94.23.203.174",
+	hmacEnabled : false
+	
+};
+
+module.exports.LocalConfig = LocalConfig;
